1919343829378388508b4144c3b2b3a023293d71
Kukushkin Alexander <alex@alexkuk.ru>
2015-10-11 01:05:07 +0300

    initial snapshot

---
+++ copy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ~/opendatacontestschools/client/index.html .
+cp ~/opendatacontestschools/client/dist/app.js dist/app.js
+cp ~/opendatacontestschools/client/vendor/css/bootstrap.css vendor/css
